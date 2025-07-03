d93fec5b65d2ca5ef9564a937c17c1ad56bcb9f7
LYNDA2203 <lindam.rita@gmail.com>
2025-07-03 12:44:08 +0530

    [lynda]-Update:Added the function to get working hours of employee

--- main.py
+++ main.py
@@ -11,4 +11,10 @@ if __name__ == "__main__":
     #UC3--calculating wages for part time employee    
     print("Part-time Wage:", employee_wage.calculate_part_time_wage())
     
+    #UC4-- Getting the working hours of an employee 
+    import random
+    emp_type = random.choice(["absent", "part_time", "full_time"])# step1: return the options 
+    hours = employee_wage.get_work_hours(emp_type)# step2: calls the function and returns values from the function
+    print(f"Employee Type: {emp_type}")# returns the employee type[full-time or part_time]
+    print("Wage:", hours * 20)# calculate wages according to the hours of employee works
    
