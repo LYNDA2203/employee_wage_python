8a8dee23694b2701bff349c06f787f847b4ecbdd
LYNDA2203 <lindam.rita@gmail.com>
2025-07-03 12:06:18 +0530

    [lynda]- Update: added functions of part-time wages of an employee

--- employee_wage.py
+++ employee_wage.py
@@ -15,3 +15,9 @@ def calculate_daily_wage():
     wage_per_hour = 20 #calculating as max of 20hours 
     full_day_hour = 8 #for full_time calculating as 8hours
     return wage_per_hour * full_day_hour
+
+#UC3--calculating wages for part time employee    
+def calculate_part_time_wage():
+    wage_per_hour = 20 #calculating max hours as 20 hours
+    part_time_hour = 4 #calculating Part_time as 4hours
+    return wage_per_hour * part_time_hour
--- main.py
+++ main.py
@@ -5,6 +5,10 @@ if __name__ == "__main__":
     print("Welcome to Employee Wage Computation Program")
     employee_wage.check_attendance()
    
-    #uc2-- Claculate Daily employee Wage 
-    print("Daily Wage:", calculate_daily_wage()) 
+    #uc2-- Calculate Daily employee Wage 
+    print("Daily Wage:", employee_wage.calculate_daily_wage()) 
+    
+    #UC3--calculating wages for part time employee    
+    print("Part-time Wage:", employee_wage.calculate_part_time_wage())
+    
    
